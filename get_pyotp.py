56eb874bbdb6aeac58ffd8462b38cb0bf02f6193
kriskingg <109910783+kriskingg@users.noreply.github.com>
2024-08-12 11:54:14 +0530

    Update get_pyotp.py

--- get_pyotp.py
+++ get_pyotp.py
@@ -2,10 +2,14 @@ import requests
 import pyotp
 import os
 import time
+import logging
 from dotenv import load_dotenv
 
-# Load environment variables from GitHub Secrets
+# Load environment variables from .env file or directly from the environment
 load_dotenv()
+
+# Setup basic logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def generate_totp():
     """Generate TOTP code using the secret key from the environment variable."""
@@ -29,32 +33,38 @@ def login_with_totp(totp):
         "application_id": os.getenv('YOUR_APPLICATION_ID')
     }
 
-    response = requests.post(url, headers=headers, json=data)
-    if response.status_code == 200:
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+
         response_data = response.json()
         access_token = response_data.get('data', {}).get('access_token')
         if access_token:
             # Save the access token as a GitHub Actions output
             with open(os.getenv('GITHUB_ENV'), 'a') as file:
                 file.write(f"ACCESS_TOKEN={access_token}\n")
-            print("Access token saved.")
+            logging.info("Access token saved.")
         else:
-            print("Access token not found in response.")
-            print("Response data:", response_data)
-    else:
-        print(f"Failed to login: {response.status_code} - {response.text}")
+            logging.error("Access token not found in response.")
+            logging.error("Response data: %s", response_data)
+    except requests.exceptions.RequestException as e:
+        logging.error(f"An error occurred: {e}")
 
 def unified_login_process():
     """Generate TOTP and perform login with it."""
-    current_totp = generate_totp()
-    
-    # Wait 3 seconds to ensure TOTP timing is correct
-    time.sleep(3)
-    
-    print(f"Generated TOTP: {current_totp}")
-    
-    # Perform login with TOTP
-    login_with_totp(current_totp)
+    try:
+        current_totp = generate_totp()
+        logging.info(f"Generated TOTP: {current_totp}")
+
+        # Wait 3 seconds to ensure TOTP timing is correct
+        time.sleep(3)
+
+        # Perform login with TOTP
+        login_with_totp(current_totp)
+    except ValueError as e:
+        logging.error(f"An error occurred during TOTP generation: {e}")
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
     unified_login_process()
