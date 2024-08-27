474d79a2a1a09a0de1f9b8463466346386c2d8c9
Chaitanya <chaitanya.behara@gmail.com>
2024-08-27 10:59:33 +0530

    update

--- chartink_script_with_db.py
+++ chartink_script_with_db.py
@@ -5,7 +5,6 @@ import logging
 from dotenv import load_dotenv
 from time import sleep
 import requests
-from bs4 import BeautifulSoup
 
 # Load environment variables from .env file
 load_dotenv()
@@ -15,9 +14,6 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(leve
 
 # Constants
 DB_FILE = "beest-orders.db"
-Charting_Link = "https://chartink.com/screener/"
-Charting_url = 'https://chartink.com/screener/process'
-condition = "( {166311} ( latest rsi(65) < latest ema(rsi(65),35) or weekly rsi(65) < weekly ema(rsi(65),35) ) )"
 YOUR_API_KEY = os.getenv('YOUR_API_KEY')
 
 def create_connection(db_file):
@@ -91,36 +87,6 @@ def get_access_token():
     except Exception as e:
         logging.error(f"Error reading access token: {e}")
         return None
-
-def fetch_chartink_data(condition):
-    """Fetch data from Chartink based on the given condition."""
-    retries = 3
-    for attempt in range(retries):
-        try:
-            with requests.Session() as s:
-                s.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-                logging.debug("Headers: {}".format(s.headers))
-                r = s.get(Charting_Link)
-                logging.debug("GET request to Charting_Link status code: {}".format(r.status_code))
-                soup = BeautifulSoup(r.text, "html.parser")
-                csrf_token = soup.select_one("[name='csrf-token']")['content']
-                s.headers.update({'x-csrf-token': csrf_token})
-                logging.debug("CSRF Token: {}".format(csrf_token))
-                logging.debug("Scan Condition: {}".format(condition))
-                response = s.post(Charting_url, data={'scan_clause': condition})
-                logging.debug("POST request to Charting_url status code: {}".format(response.status_code))
-                logging.debug("Request Data: {}".format({'scan_clause': condition}))
-                response_json = response.json()
-                logging.debug("Response JSON: {}".format(response_json))
-                if response.status_code == 200:
-                    return response_json
-                else:
-                    logging.error("Failed to fetch data with status code: {}".format(response.status_code))
-        except Exception as e:
-            logging.error("Exception during data fetch: {}".format(str(e)))
-        sleep(10)  # wait before retrying
-    logging.error("All retries failed")
-    return None
 
 def fetch_ltp_from_rupeezy(token):
     """Fetch the LTP from Rupeezy."""
@@ -213,9 +179,9 @@ def check_order_status(order_id, retries=10, delay=5):
             for order in orders:
                 if order.get('order_id') == order_id:
                     status = order.get('status')
-                    if status == 'executed':
+                    if status == 'EXECUTED':
                         return True
-                    elif status == 'rejected':
+                    elif status == 'REJECTED':
                         logging.error(f"Order {order_id} was rejected: {order}")
                         return False
 
@@ -231,66 +197,54 @@ def check_order_status(order_id, retries=10, delay=5):
     return False
 
 if __name__ == '__main__':
-    # Fetch data from Chartink
-    data = fetch_chartink_data(condition)
+    # Example data for the order
+    token = 19640  # ALPHAETF Token
+    order_quantity = 1  # Default quantity
 
-    if data:
-        alpha_data = [item for item in data['data'] if item['nsecode'] == 'ALPHAETF']
+    # Get the current price from Rupeezy
+    current_price = fetch_ltp_from_rupeezy(token)
+    if not current_price:
+        logging.error("Failed to fetch LTP from Rupeezy. Exiting.")
+        exit(1)
+    
+    logging.debug(f"LTP from Rupeezy data for ALPHAETF: {current_price}")
+    
+    order_details = {
+        "exchange": "NSE_EQ",
+        "token": token,
+        "symbol": "ALPHAETF",
+        "transaction_type": "BUY",
+        "product": "DELIVERY",
+        "variety": "RL",
+        "quantity": order_quantity,
+        "price": current_price,
+        "trigger_price": 0.00,
+        "disclosed_quantity": 0,
+        "validity": "DAY",
+        "validity_days": 1,
+        "is_amo": False
+    }
+    
+    response = trigger_order_on_rupeezy(order_details)
+    if response and response.get('status') == 'success':
+        order_id = response['data'].get('orderId')
+        logging.info(f"Order placed successfully with ID: {order_id}")
         
-        if alpha_data:
-            logging.debug(f"Filtered ALPHAETF data: {alpha_data}")
-            
-            # Get the current price from Rupeezy
-            current_price = fetch_ltp_from_rupeezy(19640)
-            if not current_price:
-                logging.error("Failed to fetch LTP from Rupeezy. Exiting.")
-                exit(1)
-            
-            logging.debug(f"LTP from Rupeezy data for ALPHAETF: {current_price}")
-            
-            order_quantity = 1  # Default quantity
-            
-            order_details = {
-                "exchange": "NSE_EQ",
-                "token": 19640,  # Token number for ALPHAETF.
-                "symbol": "ALPHAETF",
-                "transaction_type": "BUY",
-                "product": "DELIVERY",
-                "variety": "RL",
-                "quantity": order_quantity,
-                "price": current_price,
-                "trigger_price": 0.00,
-                "disclosed_quantity": 0,
-                "validity": "DAY",
-                "validity_days": 1,
-                "is_amo": False
-            }
-            
-            response = trigger_order_on_rupeezy(order_details)
-            if response and response.get('status') == 'success':
-                order_id = response['data'].get('orderId')
-                logging.info(f"Order placed successfully with ID: {order_id}")
-                
-                # Check order status to ensure it's executed
-                if check_order_status(order_id):
-                    # Store the order details in the database only if the order was executed successfully
-                    conn = create_connection(DB_FILE)
-                    if conn is not None:
-                        create_table(conn)
-                        order_entry = ("ALPHAETF", order_quantity, current_price, "BUY", "DELIVERY", current_price)
-                        insert_order(conn, order_entry)
-                        conn.close()
+    # Check order status to ensure it's executed
+    if check_order_status(order_id):
+        # Store the order details in the database only if the order was executed successfully
+        conn = create_connection(DB_FILE)
+        if conn is not None:
+            create_table(conn)
+            order_entry = ("ALPHAETF", order_quantity, current_price, "BUY", "DELIVERY", current_price)
+            insert_order(conn, order_entry)
+            conn.close()
 
-                        # Upload the database to S3
-                        upload_to_s3(DB_FILE, 'my-beest-db')
-                    else:
-                        logging.error("Failed to create the database connection.")
-                else:
-                    logging.error("Order was not executed successfully. Exiting.")
-            else:
-                logging.error(f"Failed to place order. Response: {response}")
+            # Upload the database to S3
+            upload_to_s3(DB_FILE, 'my-beest-db')
         else:
-            logging.info("No ALPHAETF data found in Chartink results. No action taken.")
+            logging.error("Failed to create the database connection.")
     else:
-        logging.error("Failed to fetch data from Chartink.")
-                     
+        logging.error("Order was not executed successfully. Exiting.")
+else:
+    logging.error(f"Failed to place order. Response: {response}")
