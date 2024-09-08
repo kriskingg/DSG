b95f22e054f9893c226fa65fce94e50031418960
Chaitanya <chaitanya.behara@gmail.com>
2024-09-08 10:45:03 +0530

    commented out auth.py as it is not in use

--- rupeezy/auth.py
+++ rupeezy/auth.py
@@ -1,26 +1,26 @@
-import os
-import logging
-from dotenv import load_dotenv
+# import os
+# import logging
+# from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+# # Load environment variables from .env file
+# load_dotenv()
 
-# Setup basic logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# # Setup basic logging
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-def get_access_token():
-    """Read access token from the file."""
-    try:
-        with open('./token/access_token.txt', 'r') as file:
-            token = file.read().strip()
-            if token:
-                logging.debug(f"Access token retrieved: '{token}'")
-            else:
-                logging.error("Access token is empty.")
-            return token
-    except FileNotFoundError:
-        logging.error("access_token.txt file not found.")
-        return None
-    except Exception as e:
-        logging.error(f"Error reading access token: {e}")
-        return None
+# def get_access_token():
+#     """Read access token from the file."""
+#     try:
+#         with open('./token/access_token.txt', 'r') as file:
+#             token = file.read().strip()
+#             if token:
+#                 logging.debug(f"Access token retrieved: '{token}'")
+#             else:
+#                 logging.error("Access token is empty.")
+#             return token
+#     except FileNotFoundError:
+#         logging.error("access_token.txt file not found.")
+#         return None
+#     except Exception as e:
+#         logging.error(f"Error reading access token: {e}")
+#         return None
